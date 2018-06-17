26749e085912114ea47c007eba7879e37e872b6a
Julian Lobe <julian@lobe.me>
2018-06-17 23:48:46 +0200

    Reduce Spam rating

--- mailer.py
+++ mailer.py
@@ -1,5 +1,6 @@
 import smtplib
 from email.mime.text import MIMEText
+from email.utils import make_msgid, formatdate
 
 from settings import smtp_host, smtp_port, smtp_user, smtp_password
 
@@ -13,4 +14,6 @@ def send_mail(to, subject, message):
     msg['From'] = smtp_user
     msg['To'] = to
     msg['Subject'] = subject
+    msg['Date'] = formatdate(localtime=True)
+    msg['Message-ID'] = make_msgid()
     server.sendmail(smtp_user, to, msg.as_string())

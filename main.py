f267e8ba985ecab01447e5c4353a79231749b41b
Julian Lobe <julian@lobe.me>
2018-06-26 23:03:47 +0200

    Configuration file instead of parameters

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import sys
 
 from checker import Checker
 from mailer import send_mail
+from settings import watched_domains, notify_mail
 
 def __main__():
     checker = Checker()
-    target_mail = sys.argv[1]
-    domain = sys.argv[2]
-    success, result = checker.check_domain(domain)
+    for domain in watched_domains:
+        success, result = checker.check_domain(domain)
 
-    if success and not result:
-        send_mail(target_mail, 'Domain available!', 'The domain {} is finally available!'.format(domain))
+        if success and not result:
+            send_mail(notify_mail, 'Domain available!', 'The domain {} is finally available!'.format(domain))
 
 __main__()
--- settings.example.py
+++ settings.example.py
@@ -3,4 +3,5 @@ smtp_port = 25
 smtp_user = 'user@example.com'
 smtp_password = 'password'
 
+notify_mail = 'user@example.com'
 watched_domains = ['example.com']
